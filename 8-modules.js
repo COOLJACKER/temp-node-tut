24d0cf54be85a193f18bc21b3d105cc6084de9e1
COOLJACKER <monchunskiha@gmail.com>
2022-05-22 11:49:37 -0600

    first commit

---
+++ 8-modules.js
@@ -0,0 +1,31 @@
+// modules
+
+const names = require('./4-names')
+
+const sayHi = require('./5-utils')
+const data = require('./6-alt-flavor')
+
+require('./7-mind-grenade')
+
+sayHi('Susan')
+sayHi(names.john)
+sayHi(names.peter)
+
+// no ./ needed because os is built-in module
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+//  method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+console.log(currentOS)
+
